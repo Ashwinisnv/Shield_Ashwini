7821f5756c1f27ff045b1b62b8ede7c804f3009a
Ashwini Vishwas <ashwinivishwas@Ashwinis-MacBook-Pro-2.local>
2018-06-16 10:10:22 -0700

    demoSaml

---
+++ demoSaml/src/main/java/com/example/demoSaml/DemoSamlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoSaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSamlApplication.class, args);
+	}
+}
+ 
